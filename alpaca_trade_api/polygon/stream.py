81223db74897094dbe31f6c0845063d96d97db91
qorrect <charlie.fats@gmail.com>
2019-06-23 23:17:01 -0500

    Setting up the next level.

--- alpaca_trade_api/polygon/stream.py
+++ alpaca_trade_api/polygon/stream.py
@@ -19,9 +19,11 @@ class Stream(object):
 
     async def connect(self, loop=None):
         servers = [
-            'nats://{}@nats1.polygon.io:31101'.format(self._api_key),
-            'nats://{}@nats2.polygon.io:31102'.format(self._api_key),
-            'nats://{}@nats3.polygon.io:31103'.format(self._api_key),
+            'nats://{}@192.168.0.172:4222'.format(self._api_key),
+
+            # 'nats://{}@nats1.polygon.io:31101'.format(self._api_key),
+            # 'nats://{}@nats2.polygon.io:31102'.format(self._api_key),
+            # 'nats://{}@nats3.polygon.io:31103'.format(self._api_key),
         ]
 
         # TODO:
